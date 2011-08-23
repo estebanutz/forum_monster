9c49b5c9ae8718ea62a49ce2d16f5f49d1dfbabd
Mike Kelley <mike@codezombie.org>
2011-08-23 14:25:03 -0600

    initial commit

---
+++ lib/forum_monster.rb
@@ -0,0 +1,2 @@
+module ForumMonster
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '2f5c0561e1b45f65f487abdda26726b979860d740842c34683d6b3a66ef8e3be37016e6e09a91916e0603bde7f1fa4f78a60c4aa12ff9c6948bd74ec8ae01762'
