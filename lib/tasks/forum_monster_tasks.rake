70cc34f8453d2bfa053c379c16d11948cd2e4b0b
Mike Kelley <mike@codezombie.org>
2011-08-23 14:30:11 -0600

    clean branch for refactoring

--- lib/tasks/forum_monster_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :forum_monster do
-#   # Task goes here
-# end
--- test/forum_monster_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ForumMonsterTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, ForumMonster
-  end
-end
